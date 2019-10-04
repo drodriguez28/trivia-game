f75f7dbc5d9b4033bd2d5f3d26c987af695c28e2
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-10-04 10:50:27 -0400

    commiting

---
+++ script.js
@@ -0,0 +1,18 @@
+const button1 = document.querySelector('.buttonMath')
+button1.addEventListener('click', () => {
+    console.log('blah')
+})
+
+const button2 = document.querySelector('.buttonUS')
+button2.addEventListener('click', () =>{
+    console.log('que onda wei')
+})
+
+const button3 = document.querySelector('.buttonSports')
+button3.addEventListener('click', () => {
+    console.log('aye wei')
+})
+const button4 = document.querySelector('.buttonAnime')
+button4.addEventListener('click', () => {
+    
+})

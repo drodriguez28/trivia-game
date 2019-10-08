77840a91e4beb18a89e2d67e3467f35827ab77fb
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-10-08 15:32:31 -0400

    made diffrent questions

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 var mathSuccess = false;
 var usSuccess = false;
 var sportsSuccess = false;
-var animeSuccess = false; 
-var musicSuccess = false; 
+var animeSuccess = false;
+var musicSuccess = false;
 
+ 
+ 
 
 function updateGameStatus(question, answer) {
     if (mathSuccess && usSuccess && sportsSuccess && animeSuccess && musicSuccess) {
-        console.log ("you won the game") 
+        console.log("you won the game")
     }
 
     if (question === "math") {
-        mathSuccess = answer; 
+        mathSuccess = answer;
         console.log("Math Success: " + mathSuccess)
     }
+    if (question === "us") {
+        usSuccess = answer;
+        console.log(" US Success: " + usSuccess)
+    }
+    if (question === "sports") {
+        sportsSuccess = answer;
+        console.log("Sports Success:" + sportsSuccess)
+    }
+    if (question === "anime") {
+        animeSuccess = answer;
+        console.log("Anime Success:" + animeSuccess)
+    }
+    if (question === "music"){
+        musicSuccess = answer;
+        console.log("Music Success:" + musicSuccess)
+
+    }
+
+
+
 }
 
 
@@ -25,7 +47,7 @@ button1.addEventListener('click', () => {
     document.getElementById("sportsID").style.display = "none";
     document.getElementById("animeID").style.display = "none";
     document.getElementById("musicID").style.display = "none";
-
+    
 })
 
 const button2 = document.getElementById('buttonUSID')
@@ -69,6 +91,8 @@ button5.addEventListener('click', () => {
     document.getElementById("animeID").style.display = "none";
     document.getElementById("musicID").style.display = "flex";
 })
+let right= 0
+
 
 
 // THIS SECTION IS JUST FOR THE MATH PART
@@ -76,17 +100,40 @@ button5.addEventListener('click', () => {
 document.getElementById('math-submit-1').addEventListener('click', MathCheckAnswerHandler);
 document.getElementById('math-submit-2').addEventListener('click', MathCheckAnswerHandler);
 document.getElementById('math-submit-3').addEventListener('click', MathCheckAnswerHandler);
+const correct = document.getElementById('correct')
+const incorrect = document.getElementById('incorrect')
 
-function MathCheckAnswerHandler(event)
-{
+
+
+
+function MathCheckAnswerHandler(event) {
     console.log(event.target.value);
 
     if (event.target.value === "yes") {
+        right++
         document.getElementById('mathSuccessID').style.backgroundColor = "lime";
+        correct.style.display = "flex";
         updateGameStatus("math", true)
+        setTimeout (() => {
+            correct.style.display = "none";
+        }, 1000)
+        document.getElementById("mathID").style.display = "none";
+        if ( right >= 5) {
+            document.getElementById('wonGame').innerHTML= " Congrats you've won !"
+        }
+
+
+        
     } else {
         document.getElementById('mathSuccessID').style.backgroundColor = "red";
+        incorrect.style.display = "flex"
         updateGameStatus("math", false)
+        setTimeout (() => {
+            location.reload()
+            incorrect.style.display = "none";
+        }, 1000)
+        
+
 
     }
 
@@ -99,37 +146,145 @@ document.getElementById('ushistory-submit-1').addEventListener('click', USCheckA
 document.getElementById('ushistory-submit-2').addEventListener('click', USCheckAnswerHandler);
 document.getElementById('ushistory-submit-3').addEventListener('click', USCheckAnswerHandler);
 
-function USCheckAnswerHandler(event)
-{
+
+
+
+function USCheckAnswerHandler(event) {
     console.log(event.target.value);
 
     if (event.target.value === "yes") {
+        right++
         document.getElementById('usSuccessID').style.backgroundColor = "lime";
+        correct.style.display = "flex";
         updateGameStatus("us", true)
+        setTimeout (() => {
+            correct.style.display = "none";
+        }, 1000)
+        document.getElementById("usID").style.display = "none";
+        if ( right >= 5) {
+            document.getElementById('wonGame').innerHTML= " Congrats you've won !"
+        }
+
+
     } else {
         document.getElementById('usSuccessID').style.backgroundColor = "red";
+        incorrect.style.display = "flex"
         updateGameStatus("us", false)
+        setTimeout (() => {
+            location.reload()
+            incorrect.style.display = "none";
+        }, 1000)
+
 
     }
 
 }
 
 // THIS SECTION IS JUST FOR THE SPORTS PART
-document.getElementById('sports-submit-1').addEventListener('click', USCheckAnswerHandler);
-document.getElementById('ushistory-submit-2').addEventListener('click', USCheckAnswerHandler);
-document.getElementById('ushistory-submit-3').addEventListener('click', USCheckAnswerHandler);
+document.getElementById('sports-submit-1').addEventListener('click', SportsCheckAnswerHandler);
+document.getElementById('sports-submit-2').addEventListener('click', SportsCheckAnswerHandler);
+document.getElementById('sports-submit-3').addEventListener('click', SportsCheckAnswerHandler);
 
-function USCheckAnswerHandler(event)
-{
+function SportsCheckAnswerHandler(event) {
     console.log(event.target.value);
 
     if (event.target.value === "yes") {
-        document.getElementById('usSuccessID').style.backgroundColor = "lime";
-        updateGameStatus("us", true)
+        right++
+        document.getElementById('sportsSuccessID').style.backgroundColor = "lime";
+        correct.style.display = "flex";
+        updateGameStatus("sports", true)
+        setTimeout (() => {
+            correct.style.display = "none";
+        }, 1000)
+        document.getElementById("sportsID").style.display = "none";
+        if ( right >= 5) {
+            document.getElementById('wonGame').innerHTML= " Congrats you've won !"
+        }
+
     } else {
-        document.getElementById('usSuccessID').style.backgroundColor = "red";
-        updateGameStatus("us", false)
+        document.getElementById('sportsSuccessID').style.backgroundColor = "red";
+        incorrect.style.display = "flex"
+        updateGameStatus("sports", false)
+        setTimeout (() => {
+            location.reload()
+            incorrect.style.display = "none";
+        }, 1000)
 
     }
 
 }
+
+// THIS SECTION IS JUST FOR THE ANIME PART
+
+document.getElementById('anime-submit-1').addEventListener('click', AnimeCheckAnswerHandler);
+document.getElementById('anime-submit-2').addEventListener('click', AnimeCheckAnswerHandler);
+document.getElementById('anime-submit-3').addEventListener('click', AnimeCheckAnswerHandler);
+
+function AnimeCheckAnswerHandler(event) {
+    console.log(event.target.value);
+
+    if (event.target.value === "yes") {
+        right++
+        document.getElementById('animeSuccessID').style.backgroundColor = "lime";
+        correct.style.display = "flex";
+        updateGameStatus("anime", true)
+        setTimeout (() => {
+            correct.style.display = "none";
+        }, 1000)
+        document.getElementById("animeID").style.display = "none";
+        if ( right >= 5) {
+            document.getElementById('wonGame').innerHTML= " Congrats you've won !"
+        }
+
+    } else {
+        document.getElementById('animeSuccessID').style.backgroundColor = "red";
+        incorrect.style.display = "flex"
+        updateGameStatus("anime", false)
+        setTimeout (() => {
+            location.reload()
+            incorrect.style.display = "none";
+        }, 1000)
+
+    }
+
+}
+
+// THIS SECTION IS JUST FOR THE MUSIC PART
+document.getElementById('music-submit-1').addEventListener('click', MusicCheckAnswerHandler);
+document.getElementById('music-submit-2').addEventListener('click', MusicCheckAnswerHandler);
+document.getElementById('music-submit-3').addEventListener('click', MusicCheckAnswerHandler);
+
+function MusicCheckAnswerHandler(event) {
+    console.log(event.target.value);
+
+    if (event.target.value === "yes") {
+        right++
+        document.getElementById('musicSuccessID').style.backgroundColor = "lime";
+        correct.style.display = "flex";
+        updateGameStatus("music", true)
+        setTimeout (() => {
+            correct.style.display = "none";
+        }, 1000)
+        document.getElementById("musicID").style.display = "none";
+        if ( right >= 5) {
+            document.getElementById('wonGame').innerHTML= " Congrats you've won !"
+        }
+
+
+    } else {
+        document.getElementById('musicSuccessID').style.backgroundColor = "red";
+        incorrect.style.display = "flex"
+        updateGameStatus("music", false)
+        setTimeout (() => {
+            location.reload()
+            incorrect.style.display = "none";
+        }, 1000)
+
+    }
+
+}
+
+
+
+
+
